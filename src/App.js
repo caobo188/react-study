b60c77b29729c2be669df370b75ca4c8437cb360
caoyabo <575142603@qq.com>
2019-01-30 18:42:42 +0800

    list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-
 
 import Home from './views/home/index.js'
 import About from './views/about/index.js'
+import Mine from './views/mine/index.js'
 
 import './App.css'
  
@@ -13,12 +14,14 @@ class App extends Component {
         <div>
           <div>
             <NavLink to="/home">首页</NavLink>
-            <NavLink to="/about">关于我们</NavLink>
+            <span>首页</span>
+            <NavLink to="/mine">个人中心</NavLink>
           </div>
           <div>
             <Route exact path="/" render={() => <Redirect to="/home" />} />
             <Route path="/home" component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/mine" component={Mine} />
           </div>
         </div>
       </Router>
